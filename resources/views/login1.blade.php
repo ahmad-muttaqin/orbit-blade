8286ed17c6abe081dc2f808df40f7950b6d16f06
Aqil Anwar <>
2022-08-18 16:31:42 +0800

    Update login1.blade.php
    ADJUST

--- resources/views/login1.blade.php
+++ resources/views/login1.blade.php
@@ -82,7 +82,8 @@
 											Not a member yet? 
 											<br> 
 											<a href="#" class="text-primary">New tenant</a> |
-											<a href="#" class="text-primary">Email activation</a>
+											<a href="register_v3.html" data-bs-toggle="modal" data-bs-target="#exampleModal1" class="text-primary"> Email Activation</a> |
+											<a href="register_v3.html" data-bs-toggle="modal" data-bs-target="#exampleModal2" class="text-primary"> Forgot Password</a> 
 										</div>
 										<hr class="bg-gray-600 opacity-2" />
 										<div class="text-gray-600 text-center  mb-0">
@@ -115,7 +116,61 @@
 					</div>
 				  </div>
 				</div>
-			  </div>	
+			  </div>
+
+			  <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+				  <div class="modal-content">
+					<div class="modal-header">
+					  <h5 class="modal-title" id="exampleModalLabel">EMAIL ACTIVATION</h5>
+					  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+					  <form>
+					  	<div class="mb-3">
+						  <label for="recipient-name" class="col-form-label">A link will be sent to your email to activate your email address. If you don't get an email within a few minutes, please re-try.</label>
+						</div>
+						<div class="mb-3">
+						  <label for="recipient-name" class="col-form-label">EMAIL</label>
+						  <input type="text" class="form-control" id="tenancy-code">
+						</div>
+					  </form>
+					</div>
+					<div class="modal-footer">
+					  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					  <button type="button" class="btn btn-primary">Submit</button>
+					</div>
+				  </div>
+				</div>
+			  </div> 
+			  
+			  <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+				  <div class="modal-content">
+					<div class="modal-header">
+					  <h5 class="modal-title" id="exampleModalLabel">FORGOT YOUR PASSWORD?</h5>
+					  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+					  <form>
+					  	<div class="mb-3">
+						  <label for="recipient-name" class="col-form-label">A password reset link will be sent to your email to reset your password. If you don't get an email within a few minutes, please re-try. </label>
+						</div>
+						<div class="mb-3">
+						  <label for="recipient-name" class="col-form-label">EMAIL</label>
+						  <input type="text" class="form-control" id="tenancy-code">
+						</div>
+					  </form>
+					</div>
+					<div class="modal-footer">
+					  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					  <button type="button" class="btn btn-primary">Submit</button>
+					</div>
+				  </div>
+				</div>
+			  </div> 
+			
+			  
 
 	<!-- END #app -->
 	
