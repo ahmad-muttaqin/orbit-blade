9b63a28cca56798582e8fec46f435751fa763c5f
Muhammad Najmuddin <75771071+mnajmuddean@users.noreply.github.com>
2022-08-29 14:15:30 +0800

    edit on dashboard 1 & 2

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -30,6 +30,9 @@
 <script src="../assets/plugins/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
 <script src="../assets/plugins/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
+
 </head>
 <body>
 	<!-- BEGIN #loader -->
@@ -553,7 +556,7 @@
 									<span class="visually-hidden">Next</span>
 								</button>
 							</div>
-</div>
+				</div>
 				
 
 	
@@ -596,9 +599,9 @@
 							
 	<div class="panel panel-inverse">
 		<div class="panel-heading mt-45px">
-								<h4 class="panel-title"><i class="fas fa-rectangle-list fa-fw"></i>Activity Log</h4>
-							</div>
-					<table id="data-table-default" class="table table-hover table-striped table-bordered align-middle mt-30px">
+			<h4 class="panel-title"><i class="fas fa-rectangle-list fa-fw"></i>Activity Log</h4>
+			</div>
+			<table id="data-table-default" class="table table-hover table-striped table-bordered align-middle ">
 				<thead>
 					<tr>
 						<th class="text-nowrap">Employee ID</th>
@@ -677,8 +680,10 @@
 						<td>-</td>
 						<td>-</td>
 					</tr>
-</table>
-<nav aria-label="Page navigation example">
+			</table>
+
+					
+		<nav aria-label="Page navigation example">
 							<ul class="pagination justify-content-center">
 								<li class="page-item">
 								<a class="page-link" href="#" aria-label="Previous">
@@ -695,246 +700,22 @@
 								</li>
 							</ul>
 							</nav>
-</div>
-		<!-- END col-4 -->
-		<!-- BEGIN col-4 -->
-		<!-- END col-4 -->
-		<!-- BEGIN col-4 -->
-		<div class="col-xl-4 col-lg-6">
-			<!-- BEGIN panel -->
+	</div>
+	
+	<!-- <div class="panel panel-inverse" style="width: 60%;">
+	<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+</div> -->
 
-			<!-- END panel -->
+<div class="row">
+		<div class="col-lg-6 panel panel-inverse">
+		<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
 		</div>
-		<!-- END col-4 -->
-	</div>
+						
+		<div class="col-lg-6 panel panel-inverse">
+		<canvas id="bar-chart-grouped" style="width:100%;max-width:600px"></canvas>
+		</div>
 </div>
-	<!-- END row -->
-		</div>
-		<!-- END #content -->
-		
-		<!-- BEGIN theme-panel -->
-		<div class="theme-panel">
-			<a href="javascript:;" data-toggle="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
-			<div class="theme-panel-content" data-scrollbar="true" data-height="100%">
-				<h5>App Settings</h5>
-				
-				<!-- BEGIN theme-list -->
-				<div class="theme-list">
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-red" data-theme-class="theme-red" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Red">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-pink" data-theme-class="theme-pink" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Pink">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-orange" data-theme-class="theme-orange" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Orange">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-yellow" data-theme-class="theme-yellow" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Yellow">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-lime" data-theme-class="theme-lime" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Lime">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-green" data-theme-class="theme-green" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Green">&nbsp;</a></div>
-					<div class="theme-list-item active"><a href="javascript:;" class="theme-list-link bg-teal" data-theme-class="" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Default">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-cyan" data-theme-class="theme-cyan" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Cyan">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-blue" data-theme-class="theme-blue" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Blue">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-purple" data-theme-class="theme-purple" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Purple">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-indigo" data-theme-class="theme-indigo" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Indigo">&nbsp;</a></div>
-					<div class="theme-list-item"><a href="javascript:;" class="theme-list-link bg-black" data-theme-class="theme-gray-600" data-toggle="theme-selector" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-container="body" data-bs-title="Black">&nbsp;</a></div>
-				</div>
-				<!-- END theme-list -->
-				
-				<div class="theme-panel-divider"></div>
-				
-				<div class="row mt-10px">
-					<div class="col-8 control-label text-dark fw-bold">
-						<div>Dark Mode <span class="badge bg-primary ms-1 py-2px position-relative" style="top: -1px;">NEW</span></div>
-						<div class="lh-14">
-							<small class="text-dark opacity-50">
-								Adjust the appearance to reduce glare and give your eyes a break.
-							</small>
-						</div>
-					</div>
-					<div class="col-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-theme-dark-mode" id="appThemeDarkMode" value="1" />
-							<label class="form-check-label" for="appThemeDarkMode">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				
-				<div class="theme-panel-divider"></div>
-				
-				<!-- BEGIN theme-switch -->
-				<div class="row mt-10px align-items-center">
-					<div class="col-8 control-label text-dark fw-bold">Header Fixed</div>
-					<div class="col-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-header-fixed" id="appHeaderFixed" value="1" checked />
-							<label class="form-check-label" for="appHeaderFixed">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-10px align-items-center">
-					<div class="col-8 control-label text-dark fw-bold">Header Inverse</div>
-					<div class="col-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-header-inverse" id="appHeaderInverse" value="1" />
-							<label class="form-check-label" for="appHeaderInverse">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-10px align-items-center">
-					<div class="col-8 control-label text-dark fw-bold">Sidebar Fixed</div>
-					<div class="col-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-sidebar-fixed" id="appSidebarFixed" value="1" checked />
-							<label class="form-check-label" for="appSidebarFixed">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-10px align-items-center">
-					<div class="col-8 control-label text-dark fw-bold">Sidebar Grid</div>
-					<div class="col-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-sidebar-grid" id="appSidebarGrid" value="1" />
-							<label class="form-check-label" for="appSidebarGrid">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-10px align-items-center">
-					<div class="col-md-8 control-label text-dark fw-bold">Gradient Enabled</div>
-					<div class="col-md-4 d-flex">
-						<div class="form-check form-switch ms-auto mb-0">
-							<input type="checkbox" class="form-check-input" name="app-gradient-enabled" id="appGradientEnabled" value="1" />
-							<label class="form-check-label" for="appGradientEnabled">&nbsp;</label>
-						</div>
-					</div>
-				</div>
-				<!-- END theme-switch -->
-				
-				<div class="theme-panel-divider"></div>
-				
-				<h5>Admin Design (5)</h5>
-				<!-- BEGIN theme-version -->
-				<div class="theme-version">
-					<div class="theme-version-item">
-						<a href="../template_html/index_v2.html" class="theme-version-link active">
-							<span style="background-image: url(../assets/img/theme/default.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_transparent/index_v2.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/transparent.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_apple/index_v2.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/apple.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_material/index_v2.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/material.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_facebook/index_v2.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/facebook.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_google/index_v2.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/google.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-				</div>
-				<!-- END theme-version -->
-				
-				<div class="theme-panel-divider"></div>
-				
-				<h5>Language Version (7)</h5>
-				<!-- BEGIN theme-version -->
-				<div class="theme-version">
-					<div class="theme-version-item">
-						<a href="../template_html/index.html" class="theme-version-link active">
-							<span style="background-image: url(../assets/img/version/html.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_ajax/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/ajax.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_angularjs/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/angular1x.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_angularjs13/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/angular10x.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="javascript:alert('Laravel Version only available in downloaded version.');" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/laravel.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_vuejs/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/vuejs.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../template_reactjs/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/reactjs.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="javascript:alert('.NET Core 3.1 MVC Version only available in downloaded version.');" class="theme-version-link">
-							<span style="background-image: url(../assets/img/version/dotnet.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-				</div>
-				<!-- END theme-version -->
-				
-				<div class="theme-panel-divider"></div>
-				
-				<h5>Frontend Design (5)</h5>
-				<!-- BEGIN theme-version -->
-				<div class="theme-version">
-					<div class="theme-version-item">
-						<a href="../../../frontend/template/template_one_page_parallax/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/one-page-parallax.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../../../frontend/template/template_e_commerce/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/e-commerce.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../../../frontend/template/template_blog/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/blog.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../../../frontend/template/template_forum/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/forum.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-					<div class="theme-version-item">
-						<a href="../../../frontend/template/template_corporate/index.html" class="theme-version-link">
-							<span style="background-image: url(../assets/img/theme/corporate.jpg);" class="theme-version-cover"></span>
-						</a>
-					</div>
-				</div>
-				<!-- END theme-version -->
-				
-				<div class="theme-panel-divider"></div>
-				
-				<a href="https://seantheme.com/color-admin/documentation/" class="btn btn-dark d-block w-100 rounded-pill mb-10px" target="_blank"><b>Documentation</b></a>
-				<a href="javascript:;" class="btn btn-default d-block w-100 rounded-pill" data-toggle="reset-local-storage"><b>Reset Local Storage</b></a>
-			</div>
-		</div>
-		<!-- END theme-panel -->
-		<!-- BEGIN scroll-top-btn -->
-		<a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top" data-toggle="scroll-to-top"><i class="fa fa-angle-up"></i></a>
-		<!-- END scroll-top-btn -->
-	</div>
-	<!-- END #app -->
+
 	
 	<!-- ================== BEGIN core-js ================== -->
 	<script src="../assets/js/vendor.min.js"></script>
@@ -960,3 +741,62 @@
     responsive: true
   });
 </script>
+
+<script>
+var xValues = ["Annual Leave", "Emergency Leave", "Sick Leave", "Unpaid Leave", "Maternity Leave"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#FFFC33",
+  "#FF3333",
+  "#7AFF33",
+  "#334FFF",
+  "#FF33F0"
+];
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Leave Employee Report"
+    }
+  }
+});
+</script>
+
+<script>
+	new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: ["20 Sept", "21 Sept", "22 Sept", "23 Sept"],
+      datasets: [
+        {
+          label: "Check-in",
+          backgroundColor: "#0030F1",
+          data: [20,30,40,50]
+        }, {
+          label: "Check-out",
+          backgroundColor: "#F10000",
+          data: [80,60,40,20]
+        }, {
+          label: "Idle",
+          backgroundColor: "#8C8C8C",
+          data: [5,30,50,20]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Attendance Summary Report'
+      }
+    }
+});
+	</script>
